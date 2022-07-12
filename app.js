d0969cefba3a51f33ebb230545bede7ccd0e74a5
YavuzKarakoc <yavuzkarakocsosyal@gmail.com>
2022-07-13 02:22:30 +0300

    add auth login logout

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose=require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute=require('./routes/pageRoute');
-const courseRoute = require('./routes/courseRoute')
-const categoryRoute = require('./routes/categoryRoute')
-
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute= require("./routes/userRoute");
 const app=express();
 
 // Connect DB
@@ -20,15 +22,35 @@ mongoose.connect('mongodb://localhost/smartedu-db',{
 app.set("view engine", "ejs");
 
 
+//Global Variable
+
+global.userIN= null;
+
+
+
 //Middlewares 
+
 app.use(express.static("public"));
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'my_keyboard_cat',
+  resave: false,
+  saveUninitialized: true,
+  store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' })
+}));
+
+app.use('*', (req, res, next) =>{
+    userIN=req.session.userID;
+    next();
+})
 
 //Routes
 app.use('/', pageRoute );
 app.use('/courses', courseRoute );
 app.use('/categories', categoryRoute  );
+app.use('/users', userRoute  );
 
 // app.get('/about', pageRoute);
 
